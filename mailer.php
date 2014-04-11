63a47171c458d57731bf78322b61dce0479f2a7a
Wilker Amorim Gomes <wilker.amorim@strategymanager.com.br>
2014-04-11 09:55:45 -0300

    Ajuste de layout

--- mailer.php
+++ mailer.php
@@ -1,49 +1,30 @@
 <?php
 
-        /************************
-        * Variáveis da página
-        *************************/
+        //1 - Definimos Para quem vai ser enviado o email
+        $para = "wilker.amorim@strategymanger.com.br";
+        //2 - resgatar o nome digitado no formulário e grava na variavel
+        $nome = $_POST['nome'];
+        //3 - resgatar o e-mail digitado no formulário e grava na variavel //
+        $email = $_POST['email'];
+        //4 - resgatar o assunto digitado no formulário e grava na variavel //
+        $fone = $_POST['fone'];
 
-        $mailto   = "marketing@strategymanager.com.br"; // Enter your mail addres here.
-        $name     = ucwords($_POST['name']);
-        $subject  = "$name está interessado na solução Valorae."; // Definir o assunto aqui.
-        $email    = $_POST['email'];
-        $fone  = $_POST['fone'];
+        //6 - agora inserimos as codificações corretas e tudo mais.
+        $headers = "Content-Type:text/html; charset=UTF-8\n";
+        $headers .= "From: strategymanger.com.br<wilker.amorim@strategymanger.com.br>\n";
 
-        if(strlen($_POST['name']) < 1 ){
-        echo  'email_error';
-        }
+        //Vai ser //mostrado que o email partiu deste email e seguido do nome
+        $headers .= "X-Sender: <wilker.amorim@strategymanger.com.br>\n";
 
-        else if(strlen($email) < 1 ) {
-        echo 'email_error';
-        }
+        //email do servidor //que enviou
+        $headers .= "X-Mailer: PHP v".phpversion()."\n";
+        $headers .= "X-IP: ".$_SERVER['REMOTE_ADDR']."\n";
+        $headers .= "Return-Path: <wilker.amorim@strategymanger.com.br>\n";
 
-        else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", $email)) {
-        echo 'email_error';
-        }
+        //caso a msg //seja respondida vai para este email.
+        $headers .= "MIME-Version: 1.0\n";
+        mail($para, $nome, $email, $fone);
 
-        else if(strlen($fone) < 1 ){
-        echo 'email_error';
+        //função que faz o envio do email.
 
-        } else {
-
-        // NOW SEND THE ENQUIRY
-
-        $email_message="\n\n" .
-        "Nome: " .
-        ucwords($name) .
-        "\n" .
-        "E-mail: " .
-        $email .
-        "\n" .
-        "Telefone: " .
-        "\n" .
-        $fone .
-        "\n" .
-        "\n\n" ;
-
-        $email_message = trim(stripslashes($email_message));
-        mail($mailto, $subject, $email_message, "De: \"$vname\" <".$email.">\nReply-To: \"".ucwords($name)."\" <".$email.">\nX-Mailer: PHP/" . phpversion() );
-
-        }
-?>
+        ?>
